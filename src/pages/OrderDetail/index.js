7fce31e3b4ce778b9757dd7d350d1bbe426fd151
Yosada Dede <masyosad@gmail.com>
2020-12-22 10:20:01 +0700

    yosadade

--- src/pages/OrderDetail/index.js
+++ src/pages/OrderDetail/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import {Header, ItemListFood, ItemValue, Button} from '../../components';
 import {FoodDummy1} from '../../assets';
 
-const OrderDetail = ({navigation}) => {
+const OrderDetail = () => {
   return (
     <View style={styles.page}>
       <Header
@@ -12,41 +12,53 @@ const OrderDetail = ({navigation}) => {
         subTitle="You deserve better meal"
         onBack={() => {}}
       />
-      <View style={styles.content}>
-        <Text style={styles.label}>Item Order</Text>
-        <ItemListFood
-          type="order-summary"
-          item={14}
-          image={FoodDummy1}
-          name="Soup Bumil"
-          price="297.000"
-        />
-        <Text style={[styles.label, {marginTop: 16}]}>Details Transaction</Text>
-        <ItemValue label="Cherry Healthy" value="IDR 1.839.000" />
-        <ItemValue label="Drive" value="IDR 50.000" />
-        <ItemValue label="Tax 10%" value="IDR 183.900" />
-        <ItemValue
-          label="Total Price"
-          value="IDR 2.000.000"
-          valueColor="#1ABC9C"
-        />
-      </View>
-      <View style={styles.content}>
-        <Text style={styles.label}>Deliver To:</Text>
-        <ItemValue label="Name" value="Yosada Dede" />
-        <ItemValue label="Phone No." value="0899 1989 089" />
-        <ItemValue label="Address" value="Sukorame, Dlingo" />
-        <ItemValue label="House No." value="17" />
-        <ItemValue label="City" value="Yogyakarta" />
-      </View>
-      <View style={styles.button}>
-        <Button
-          title="Cancel My Order"
-          onPress={() => {}}
-          bgColor="#D9435E"
-          color="#FFFFFF"
-        />
-      </View>
+      <ScrollView style={{flex: 1}}>
+        <View style={styles.content}>
+          <Text style={styles.label}>Item Order</Text>
+          <ItemListFood
+            type="order-summary"
+            item={14}
+            image={FoodDummy1}
+            name="Soup Bumil"
+            price="297.000"
+          />
+          <Text style={[styles.label, {marginTop: 16}]}>
+            Details Transaction
+          </Text>
+          <ItemValue label="Cherry Healthy" value="IDR 1.839.000" />
+          <ItemValue label="Drive" value="IDR 50.000" />
+          <ItemValue label="Tax 10%" value="IDR 183.900" />
+          <ItemValue
+            label="Total Price"
+            value="IDR 2.000.000"
+            valueColor="#1ABC9C"
+          />
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.label}>Deliver To:</Text>
+          <ItemValue label="Name" value="Yosada Dede" />
+          <ItemValue label="Phone No." value="0899 1989 089" />
+          <ItemValue label="Address" value="Sukorame, Dlingo" />
+          <ItemValue label="House No." value="17" />
+          <ItemValue label="City" value="Yogyakarta" />
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.label}>Order Status</Text>
+          <ItemValue
+            label="Total Price"
+            value="IDR 2.000.000"
+            valueColor="#1ABC9C"
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="Cancel My Order"
+            onPress={() => {}}
+            bgColor="#D9435E"
+            color="#FFFFFF"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -54,7 +66,9 @@ const OrderDetail = ({navigation}) => {
 export default OrderDetail;
 
 const styles = StyleSheet.create({
-  page: {},
+  page: {
+    flex: 1,
+  },
   label: {
     fontSize: 14,
     marginBottom: 8,
@@ -69,6 +83,6 @@ const styles = StyleSheet.create({
   },
   button: {
     paddingHorizontal: 24,
-    marginTop: 24,
+    marginVertical: 24,
   },
 });
